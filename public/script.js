5e27a3ffdf3529d8105613357b1afb9a6f64c7e7
mehranmohazeb-cell <mehranmohazeb@gmail.com>
2026-09-07 20:57:15 +0330

    Update script.js

--- public/script.js
+++ public/script.js
@@ -15,7 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
   applyContactInfo();
   setupMobileMenu();
   setupFooterYear();
-  loadStoreProducts();
 });
 
 function applyContactInfo() {
